32bcba1b53bdf9efdebe04cebc7e2a32c5efd18b
Mike Bostock <mbostock@gmail.com>
2016-04-08 10:15:14 -0700

    Handle coincident nodes.

--- src/add.js
+++ src/add.js
@@ -60,10 +60,10 @@ export default function(x, y, data) {
     // collapse all the points below the given depth to a linked list.
     xm = x1 - x0;
     switch (i = (y < (y0 + y1) / 2) << 1 | (x < (x0 + x1) / 2)) {
-      case 0: do parent = new Array(4), parent[i] = node, node = parent, xm *= 2, x1 = x0 + xm, y1 = y0 + xm; while (x >= x1 || y >= y1); break;
-      case 1: do parent = new Array(4), parent[i] = node, node = parent, xm *= 2, x0 = x1 - xm, y1 = y0 + xm; while (x0 > x || y >= y1); break;
-      case 2: do parent = new Array(4), parent[i] = node, node = parent, xm *= 2, x1 = x0 + xm, y0 = y1 - xm; while (x >= x1 || y0 > y); break;
-      case 3: do parent = new Array(4), parent[i] = node, node = parent, xm *= 2, x0 = x1 - xm, y0 = y1 - xm; while (x0 > x || y0 > y); break;
+      case 0: do parent = new Array(4), parent[i] = node, node = parent, x1 = x0 + (xm *= 2), y1 = y0 + xm; while (x >= x1 || y >= y1); break;
+      case 1: do parent = new Array(4), parent[i] = node, node = parent, x0 = x1 - (xm *= 2), y1 = y0 + xm; while (x0 > x || y >= y1); break;
+      case 2: do parent = new Array(4), parent[i] = node, node = parent, x1 = x0 + (xm *= 2), y0 = y1 - xm; while (x >= x1 || y0 > y); break;
+      case 3: do parent = new Array(4), parent[i] = node, node = parent, x0 = x1 - (xm *= 2), y0 = y1 - xm; while (x0 > x || y0 > y); break;
     }
 
     node[3 - i] = leaf(point);
@@ -83,8 +83,14 @@ export default function(x, y, data) {
   // If the new point is in an empty node, just add it.
   if (!(point0 = parent.point)) return parent[i] = leaf(point);
 
+  // If the new point is exactly coincident with the specified point, add it.
+  if (x === point0[0] && y === point0[1]) {
+    point.next = point0;
+    parent.point = point;
+    return;
+  }
+
   // Otherwise, split the leaf node until the old and new point are separated.
-  // TODO Test if the old and new point are exactly coincident to abort early.
   parent.point = undefined;
   while (i === (i0 = (point0[1] >= ym) << 1 | (point0[0] >= xm))) {
     parent = parent[i] = new Array(4);
